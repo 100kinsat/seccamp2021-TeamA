92d324cb73f177b7a73cc450e4af14ae2e150e63
hatena75 <hatena75@outlook.jp>
2021-08-23 22:14:02 +0900

    SDカード用ライブラリの追加

---
+++ ソースコード/sdLibrary/sdLibrary.ino
@@ -0,0 +1,38 @@
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
+ */
+#include "sd_lib.hpp"
+
+MySD mysd = MySD();
+
+void setup(){
+    Serial.begin(115200);
+    mysd.listDir(SD, "/", 0);
+    mysd.createDir(SD, "/mydir");
+    mysd.listDir(SD, "/", 0);
+    mysd.removeDir(SD, "/mydir");
+    mysd.listDir(SD, "/", 2);
+    mysd.writeFile(SD, "/hello.txt", "Hello ");
+    mysd.appendFile(SD, "/hello.txt", "World!\n");
+    mysd.readFile(SD, "/hello.txt");
+    mysd.deleteFile(SD, "/foo.txt");
+    mysd.renameFile(SD, "/hello.txt", "/foo.txt");
+    mysd.readFile(SD, "/foo.txt");
+    mysd.testFileIO(SD, "/test.txt");
+    mysd.displaySpaceInfo();
+}
+
+void loop(){
+
+}
